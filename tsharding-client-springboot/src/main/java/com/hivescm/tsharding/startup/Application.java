31daee36798800c8312b11c71634c624c9065fd9
luanshoushen <luanshoushen@beescm.cn>
2017-05-27 15:36:51 +0800

    update

--- tsharding-client-springboot/src/main/java/com/hivescm/tsharding/startup/Application.java
+++ tsharding-client-springboot/src/main/java/com/hivescm/tsharding/startup/Application.java
@@ -5,12 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.ImportResource;
-
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@ComponentScan(basePackages = "com.hivescm")
-//@ImportResource(locations = { "classpath:spring-tsharding.xml" })
-@ComponentScan("com.hivescm")
+////import org.springframework.context.annotation.ImportResource;
+//
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+////@ImportResource(locations = { "classpath:spring-tsharding.xml" })
+//@ComponentScan("com.hivescm")
 public class Application {
 	public static void main(String[] args) {
 		ApplicationContext context= SpringApplication.run(Application.class, args);
