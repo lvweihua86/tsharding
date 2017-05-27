a8f3b7b173acb066d112526a6a08427d04aa21d9
luanshoushen <luanshoushen@beescm.cn>
2017-05-27 12:01:15 +0800

    update

--- tsharding-client-springboot/src/test/java/com/hivescm/tsharding/config/MySpringBootMapperScannerWithSharding.java
+++ tsharding-client-springboot/src/test/java/com/hivescm/tsharding/config/MySpringBootMapperScannerWithSharding.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration()
 public class MySpringBootMapperScannerWithSharding extends SpringBootMapperScannerWithSharding {
-
+	/**
+	 * 装载Mapper xml资源配置
+	 */
 	@Override
 	public String loadMapperXmlResource() {
 		return "classpath*:sqlmap/*/*-mapper.xml";
--- tsharding-client-springboot/src/test/java/com/hivescm/tsharding/config/MySpringBootMapperShardingInitializer.java
+++ tsharding-client-springboot/src/test/java/com/hivescm/tsharding/config/MySpringBootMapperShardingInitializer.java
@@ -1,13 +1,14 @@
 package com.hivescm.tsharding.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 import com.mogujie.service.tsharding.mapper.ShopOrderMapper;
 import com.mogujie.service.tsharding.mapper.UserMapper;
-@Order(10)
 @Configuration()
 public class MySpringBootMapperShardingInitializer extends SpringBootMapperShardingInitializer {
+	/**
+	 * 装载增强Mapper接口
+	 */
 	@Override
 	public Class<?>[] loadEnhancedMapper() {
 		return new Class<?>[] { ShopOrderMapper.class, UserMapper.class };
