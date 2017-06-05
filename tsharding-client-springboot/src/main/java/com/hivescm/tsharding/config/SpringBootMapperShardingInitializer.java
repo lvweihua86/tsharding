5c146508af3daca63f4a543da82559aef88fac5f
luanshoushen <luanshoushen@beescm.cn>
2017-06-05 11:41:32 +0800

    兼容没有增强Mapper的处理

--- tsharding-client-springboot/src/main/java/com/hivescm/tsharding/config/SpringBootMapperShardingInitializer.java
+++ tsharding-client-springboot/src/main/java/com/hivescm/tsharding/config/SpringBootMapperShardingInitializer.java
@@ -1,5 +1,8 @@
 package com.hivescm.tsharding.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 
@@ -9,8 +12,21 @@ import com.mogujie.trade.tsharding.route.orm.MapperShardingInitializer;
 @Configuration()
 @Order(10)
 public class SpringBootMapperShardingInitializer extends MapperShardingInitializer {
+	private boolean isNeedEnhancedMapper = false;
+
 	public SpringBootMapperShardingInitializer() {
 		super();
-		setNeedEnhancedClasses(ScanMapperUtils.scanMapper().buildEnhancedMapper());
+		String enhancedMapper = ScanMapperUtils.scanMapper().buildEnhancedMapper();
+		if (StringUtils.isNotBlank(enhancedMapper)) {
+			isNeedEnhancedMapper=true;
+			setNeedEnhancedClasses(enhancedMapper);
+		}
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		if (isNeedEnhancedMapper) {
+			super.setApplicationContext(applicationContext);
+		}
 	}
 }
--- tsharding-client-springboot/src/main/java/com/hivescm/tsharding/startup/Application.java
+++ tsharding-client-springboot/src/main/java/com/hivescm/tsharding/startup/Application.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-////import org.springframework.context.annotation.ImportResource;
-//
+//////import org.springframework.context.annotation.ImportResource;
+////
 //@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-////@ImportResource(locations = { "classpath:spring-tsharding.xml" })
+//////@ImportResource(locations = { "classpath:spring-tsharding.xml" })
 //@ComponentScan("com.hivescm")
 public class Application {
-	public static void main(String[] args) {
-		ApplicationContext context= SpringApplication.run(Application.class, args);
-		System.out.println(context);
-	}
+//	public static void main(String[] args) {
+//		ApplicationContext context= SpringApplication.run(Application.class, args);
+//		System.out.println(context);
+//	}
 }
