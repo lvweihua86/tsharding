b91d956c3504e6c90bf7207292067cf914f2cc7b
luanshoushen <luanshoushen@beescm.cn>
2017-06-12 15:47:40 +0800

    增加自动激活tsharding拦截器配置

---
+++ tsharding-spring-boot/src/main/java/com/hivescm/tsharding/config/TshardingAutoEnableInterceptor.java
@@ -0,0 +1,32 @@
+package com.hivescm.tsharding.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+
+import com.hivescm.tsharding.cache.CacheMapperHandlerInterceptor;
+import com.mogujie.tsharding.filter.HandlerInterceptorAdapterFactory;
+
+/***
+ * 使用Spring Boot注册Tsharding拦截器
+ * 
+ * @author SHOUSHEN LUAN
+ *
+ */
+@Configuration
+public class TshardingAutoEnableInterceptor implements ApplicationContextAware {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TshardingAutoEnableInterceptor.class);
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		LOGGER.warn("Enable TshardingAutoEnableInterceptor start......");
+		AutowireCapableBeanFactory beanFactory = context.getAutowireCapableBeanFactory();
+		CacheMapperHandlerInterceptor interceptor = beanFactory.createBean(CacheMapperHandlerInterceptor.class);
+		HandlerInterceptorAdapterFactory.registerInterceptor(interceptor);
+		LOGGER.warn("Enable TshardingAutoEnableInterceptor done......");
+	}
+}
