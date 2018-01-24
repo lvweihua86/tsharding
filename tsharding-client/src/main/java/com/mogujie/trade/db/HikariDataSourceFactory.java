cd3ebe3862726cd516bd2ef5ef280a6ea090e1d3
栾守申 <luanshoushen@beescm.cn>
2018-01-24 17:51:29 +0800

    增加初始化连接池日志

--- tsharding-client/src/main/java/com/mogujie/trade/db/HikariDataSourceFactory.java
+++ tsharding-client/src/main/java/com/mogujie/trade/db/HikariDataSourceFactory.java
@@ -2,6 +2,7 @@ package com.mogujie.trade.db;
 
 import java.sql.SQLException;
 
+import com.mogujie.trade.utils.TShardingLog;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -16,30 +17,29 @@ import com.zaxxer.hikari.HikariDataSource;
  */
 public class HikariDataSourceFactory implements DataSourceFactory<HikariDataSource> {
 
-    @Override
-    public HikariDataSource getDataSource(DataSourceConfig config) throws SQLException {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(config.getUrl());
-        hikariConfig.setUsername(config.getUsername());
-        hikariConfig.setPassword(config.getPassword());
-        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
-        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        hikariConfig.setIdleTimeout(30000);
-        hikariConfig.setMaximumPoolSize(config.getMaxPoolSize());
-        hikariConfig.setMinimumIdle(config.getMinPoolSize());
-        hikariConfig.setConnectionTimeout(3000);
-        hikariConfig.setMaxLifetime(150000);
-        if (config.getDriver() != null) {
-            hikariConfig.setDriverClassName(config.getDriver());
-        }
-        HikariDataSource ds = new HikariDataSource(hikariConfig);
-//        ds.setMaximumPoolSize(config.getMaxPoolSize());
-//        ds.setMinimumIdle(config.getMinPoolSize());
-//        ds.setConnectionTimeout(3000);
-//        ds.setIdleTimeout(30000);
-//        ds.setMaxLifetime(150000);
-        return ds;
-    }
+	@Override
+	public HikariDataSource getDataSource(DataSourceConfig config) throws SQLException {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setJdbcUrl(config.getUrl());
+		hikariConfig.setUsername(config.getUsername());
+		hikariConfig.setPassword(config.getPassword());
+		hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+		hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+		hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		hikariConfig.setIdleTimeout(30000);
+		hikariConfig.setMaximumPoolSize(config.getMaxPoolSize());
+		hikariConfig.setMinimumIdle(config.getMinPoolSize());
+		hikariConfig.setConnectionTimeout(3000);
+		hikariConfig.setMaxLifetime(150000);
+		if (config.getDriver() != null) {
+			hikariConfig.setDriverClassName(config.getDriver());
+		}
+		TShardingLog.getLogger().warn(
+				"dataSource.init|url:{},maxPoolSize:{},minPoolSize:{},"
+						+ "connectionTimeout:3000ms,idleTimeout:30000ms,maxLifetime:150000",
+				config.getUrl(), config.getMaxPoolSize(), config.getMinPoolSize());
+		HikariDataSource ds = new HikariDataSource(hikariConfig);
+		return ds;
+	}
 
 }
